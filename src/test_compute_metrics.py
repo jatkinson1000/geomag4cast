3810c074de714fd32b6ad22ad7b1fd2ebde13716
Jack Atkinson <jacson@dhcp-10-248-106-52.eduroam.wireless.private.cam.ac.uk>
2022-09-23 15:56:20 +0100

    Added test for NN input averages.

--- src/test_compute_metrics.py
+++ src/test_compute_metrics.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
-import preprocess
+import compute_metrics
+from maginput import MagInput
+import numpy as np
 
 class Test_Preprocess:
-    def test_something(self):
-        pass
+    def test_averaging(self):
+        onearr = np.ones(int(60 * 24 * 20 / 5))
+        testmagin = MagInput
+        testmagin.Kp = onearr
+        testmagin.Dst = onearr
+        testmagin.dens = onearr
+        testmagin.velo = onearr
+        testmagin.Pdyn = onearr
+        testmagin.ByIMF = onearr
+        testmagin.BzIMF = onearr
+        testmagin.W3 = onearr
+
+        input_i, output_i = compute_metrics.preprocess_data(testmagin, 1000, 100)
+        np.testing.assert_array_equal(input_i[:6], np.asarray([1.0, 1.0, 1.0, 1.0, 1.0, 1.0]))
