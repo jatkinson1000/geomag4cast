c1b4c7c6e3a266ea9f37ae8bfbf8920326d45d15
Aaron Wienkers <aaron.wienkers@gmail.com>
2022-09-23 14:24:58 +0100

    blah

--- src/examples.py
+++ src/examples.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     mag_input, _, _, _ = MagInput.from_QD_file('../data/QDInput_Jul_2000.dat')
 
     # calculate_G(mag_input)
-    calculate_W(mag_input, TSParams.load_json_params('ts05_params_5min.json'))
+    #calculate_W(mag_input, TSParams.load_json_params('ts05_params_5min.json'))
 
     fig, ax = plt.subplots(4, 1, sharex='col')
 
